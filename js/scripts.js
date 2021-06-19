4c4994b22c959984829d39053bfffaf186a68b9f
Zeinab Jama <zejama99@gmail.com>
2021-06-19 00:23:04 -0700

    define question values in scripts and add possible outputs to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,21 @@ $(document).ready(function() {
   $("#quiz").submit(function(event) {
     event.preventDefault();
 
+    const q1 = $("input:radio[name=question1]:checked").val();
+    const q2 = $("#question2").val();
+    const q3 = $("#question3").val();
+    const q4 = $("#question4").val();
+    const q5 = $("#question5").val();
+  
+  let result;
+
+  if (q2 = "1") {
+    $("#suggestion1").show();
+  } else {
+    
+  }
+  
+  ("#output").text(result);
+
   });
 });
