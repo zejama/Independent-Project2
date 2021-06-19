64b05a89169fc425cb193cc70b08524e3087b43e
Zeinab Jama <zejama99@gmail.com>
2021-06-19 02:08:56 -0700

    give an output for a certain combonation of answers

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,10 @@ $(document).ready(function() {
     const q4 = $("#question4").val();
     const q5 = $("#question5").val();
   
-  if (q1 === "Yes") {
+  if (q1, q2, q3, q4, q5 === "Yes", "2", "6", "8", "9", "11") {
     $('.suggestion1').show();
   } else {
     $('.error').show();
   }
-  
-  });
+});
 });
