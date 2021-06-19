0d8b6ba3afca8de96958c20d700c77a64a0ac553
Zeinab Jama <zejama99@gmail.com>
2021-06-19 16:41:32 -0700

    display two outputs for 2 diff language suggestions. Am working on third

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,29 @@ $(document).ready(function() {
     const q4 = $("#question4").val();
     const q5 = $("#question5").val();
   
-    if (q1, q2, q3, q4, q5 === "Yes", "1", "4", "7", "10") {
-    $('.suggestion1').show();
-  } else if (q1, q2, q3, q4, q5 === "No", "2", "5", "8", "11") {
-    $('.suggestion2').show();
-  } else if (q1, q2, q3, q4, q5 === "Neither", "3", "6", "9", "12") { 
-    $('.suggestion3').show();
+    if (q1 === "Yes") {
+      $('.suggestion1').show();
+    } else if (q1, q3 === "No", "5") {
+      $('.suggestion2').show();
+    } else if (q1, q2, q3, q4, q5 === "Neither", "3", "6", "9", "12") { 
+      $('.suggestion1').hide();
+      $('.suggestion2').hide();
+      $('.suggestion3').show();
     } else {
-    $('.suggestion3').show();
-  }
+      $('.error').show();
+    }
+    
+    
+    
+    // if (q1, q2, q3, q4, q5 === "No", "1", "4", "7", "10") {
+   // $('.suggestion1').show();
+  // } else if (q1, q2, q3, q4, q5 === "Yes", "2", "5", "8", "11") {
+    // $('.suggestion1').hide();
+    // $('.suggestion2').show();
+  // } else if (q1, q2, q3, q4, q5 === "Neither", "3", "6", "9", "12") { 
+    // $('.suggestion2').hide();
+    // } else {
+    // $('.suggestion3').show();
+  // }
 });
 });
