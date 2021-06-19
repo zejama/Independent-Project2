0a98380abb19a8b65f6866bd2deb6d8ccf446d48
Zeinab Jama <zejama99@gmail.com>
2021-06-19 03:02:12 -0700

    add an extra option to each question

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,12 @@ $(document).ready(function() {
     const q4 = $("#question4").val();
     const q5 = $("#question5").val();
   
-  if (q1, q2, q3, q4, q5 === "Yes", "2", "6", "8", "9", "11") {
+  if (q1, q2, q3, q4, q5 === "Yes", "2", "6", "8", "11") {
     $('.suggestion1').show();
+  } else if (q2, q3, q4, q5 === "3", "5", "8", "12") {
+    $('.suggestion2').show();
+  } else if (q1, q2, q3, q4, q5 === "No", "2", "5", "9", "11") { 
+    $('.suggestion3').show();
   } else {
     $('.error').show();
   }
