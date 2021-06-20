eebcc2528d7c13124813dd75d52a27f063e307bb
Zeinab Jama <zejama99@gmail.com>
2021-06-19 20:35:20 -0700

    output 3 different language suggestions to the user after submission

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,15 @@ $(document).ready(function() {
     const q4 = $("#question4").val();
     const q5 = $("#question5").val();
   
-    if (q1 === "Yes" & q3 === "5" & q5 === "9") {
+    if (q1 === "Yes" & q3 === "5" & q4 === "9") {
       $('.suggestion1').show();
     } else if (q1 === "Yes") {
       $('.suggestion1').hide();
       $('.error').show();
     } else if (q3 === "5") {
+      $('.suggestion1').hide();
+      $('.error').show();
+    } else if (q4 === "9") {
       $('.suggestion1').hide();
       $('.error').show();
     } else if (q1 === "No" & q2 === "3" & q4 === "7") { 
@@ -27,7 +30,20 @@ $(document).ready(function() {
     } else if (q4 === "7") {
       $('.suggestion2').hide();
       $('.error').show();
-    } else if 
+    } else if (q1 === "Neither" & q2 === "1" & q4 === "8") {
+      $('.suggestion3').show();
+    } else if (q1 === "Neither"){
+      $('.suggestion3').hide();
+      $('.error').show();
+    } else if (q2 === "1") {
+      $('.suggestion3').hide();
+      $('.error').show();
+    } else if (q4 === "8") {
+      $('.suggestion3').hide();
+      $('.error').show();
+    } else {
+      $('.error').show();
+    }
     
     
     
